5156623f1c34914dac8256649baa72346cf4ecd9
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-06-03 00:05:38 +0530

    Debugged The Date

--- utils/messages.js
+++ utils/messages.js
@@ -3,7 +3,7 @@ function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format("h:mm a"),
+    time: moment().format("hh:mm a"),
   };
 }
 module.exports = formatMessage;
