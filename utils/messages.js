c9b6277fef0a95b40d08a3dcc23d0f39ed8bd898
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-06-03 12:46:00 +0530

    commit

--- utils/messages.js
+++ utils/messages.js
@@ -2,10 +2,6 @@ function formatMessage(username, text) {
   return {
     username,
     text,
-    time: new Date().toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    }),
   };
 }
 module.exports = formatMessage;
