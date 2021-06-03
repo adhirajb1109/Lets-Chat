0cfee24a10f685dd32878139dee4dcf23826ecd9
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-06-03 12:48:43 +0530

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -25,7 +25,7 @@ chatForm.addEventListener("submit", (event) => {
 function outputMessage(message) {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username} <span> ${message.time}</span></p>
+  div.innerHTML = `<p class="meta">${message.username}</p>
                    <p class="text">
                       ${message.text}
                     </p>`;
