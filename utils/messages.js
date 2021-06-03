7f60f89f3ebad9477b889fcd72da7246a3f73c38
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-06-03 12:23:29 +0530

    Deleted Moment.js Library

--- utils/messages.js
+++ utils/messages.js
@@ -1,9 +1,11 @@
-const moment = require("moment");
 function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format("hh:mm a"),
+    time: new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
   };
 }
 module.exports = formatMessage;
